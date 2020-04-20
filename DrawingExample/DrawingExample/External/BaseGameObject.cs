2176765d923dce4ccd55faa3296aa45b0c3bc52f
Toxic18 <49251120+Toxic18@users.noreply.github.com>
2020-04-20 16:03:21 -0400

    Uploaded to GitHub

---
+++ DrawingExample/DrawingExample/External/BaseGameObject.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace DrawingExample
+{
+    public class BaseGameObject 
+    {
+        public Vector2 ScreenSize = new Vector2(1280, 960);
+
+        public bool isActive = true;
+        public Vector2 Position = Vector2.Zero;
+        public Vector2 Scale = Vector2.Zero;
+        public float Rotation = 0;
+        public Vector2 Velocity;
+        public bool HasMaxiumVelocity = false; 
+        public float MaxiumVelocity = float.MaxValue; 
+        
+
+        public BaseGameObject()
+        {
+            GameApp.instance.SceneList.Add(this); 
+
+            InitalizeObject(); 
+        }
+
+        public virtual void InitalizeObject()
+        {
+
+        }
+
+        public void ObjectUpdate(GameTime gameTime)
+        {
+            if (isActive)
+            {
+
+                Update(gameTime);
+                if (HasMaxiumVelocity)
+                { 
+                    ScrubVelocity();
+                }
+
+                float gT = (float)gameTime.ElapsedGameTime.TotalSeconds;
+                Position += Velocity * gT;
+            }
+        }
+
+        public void ScrubVelocity()
+        {
+            if ( Velocity.Length() > MaxiumVelocity)
+            {
+                Velocity.Normalize(); 
+                Velocity *= MaxiumVelocity; 
+            }
+        }
+
+        public  virtual void Update(GameTime gameTime)
+        {
+            
+
+        }
+
+        public void ObjectDraw(SpriteBatch spriteBatch)
+        {
+            if (isActive)
+            {
+                Draw(spriteBatch);
+            }
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+
+
+        }
+
+        public void Destroy()
+        {
+            OnDestroy(); 
+            GameApp.instance.SceneList.Remove(this);
+        }
+
+        public virtual void OnDestroy()
+        {
+            isActive = false;
+        }
+
+
+    }
+}
---
+++ DrawingExample/DrawingExample/GameMode.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using LineDraw; 
+
+namespace DrawingExample
+{
+    class GameMode : GameApp
+    {
+        float offset = 0;
+        Grid2D theGrid;
+
+        /// <summary>
+        /// Public contstructor... Does need to do anything at all. Those are the best constructors. 
+        /// </summary>
+        public GameMode() : base() { }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            // Setting up Screen Resolution
+            // Read more here: http://rbwhitaker.wikidot.com/changing-the-window-size
+            graphics.PreferredBackBufferWidth = 640;
+            graphics.PreferredBackBufferHeight = 480;
+            graphics.IsFullScreen = false;
+            graphics.ApplyChanges();
+
+            theGrid = new Grid2D();
+            IsMouseVisible = true; 
+      
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+           
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+            // If you create textures on the fly, you need to unload them. 
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void GameUpdate(GameTime gameTime)
+        {
+
+            offset += gameTime.ElapsedGameTime.Milliseconds;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+
+            int deltaScrollWheel = mousePrevious.ScrollWheelValue - mouseCurrent.ScrollWheelValue; 
+            if (deltaScrollWheel != 0)
+            {
+                theGrid.GridSize += (Math.Abs(deltaScrollWheel) / deltaScrollWheel) * 2; 
+            }
+
+            //mouseCurrent.MiddleButton
+            if (MouseButtonIsPressed("MiddleButton"))
+            {
+                theGrid.Origin = mouseCurrent.Position.ToVector2(); 
+            }
+
+            // Toggle Drawing Lines
+            if (IsKeyPressed(Keys.Q))
+            {
+                theGrid.isDrawingLines = !theGrid.isDrawingLines; 
+            }
+
+            // Toggle Drawing Divison Lines
+            if (IsKeyPressed(Keys.W))
+            {
+                theGrid.isDrawingDivsions = !theGrid.isDrawingDivsions;
+            }
+
+            // Toggle Drawing Axis Lines
+            if (IsKeyPressed(Keys.E))
+            {
+                theGrid.IsDrawingAxis = !theGrid.IsDrawingAxis;
+            }
+
+            // Move Point to check in Triangle
+            if (IsKeyPressed(Keys.A))
+            {
+                
+            }
+
+            // Move Point A on Triangle
+            if (IsKeyPressed(Keys.S))
+            {
+
+            }
+
+            // Move Point B on Triangle
+            if (IsKeyPressed(Keys.D))
+            {
+
+            }
+
+            // Move Point C on Triangle
+            if (IsKeyPressed(Keys.F))
+            {
+
+            }
+
+            // Toggle Showing Controls
+            if (IsKeyPressed(Keys.Tab))
+            {
+
+            }
+
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        //protected override void Draw(GameTime gameTime)
+        //{
+        //    GraphicsDevice.Clear(clearColor);
+
+        //    spriteBatch.Begin();
+
+            
+
+        //    theGrid.Draw(spriteBatch);
+
+        //    spriteBatch.End();
+
+        //    base.Draw(gameTime);
+        //}
+    }
+}
